b4e0c3de644a7d753fa3e9bfd31f38b29139a531
fede3alvarez <fede3alvarez@gmail.com>
2020-04-14 22:37:16 -0400

    Implemented Stepper Motor on IRQ + code clean-up

--- ECE362/final_project/Sources/main.asm
+++ ECE362/final_project/Sources/main.asm
@@ -3,44 +3,57 @@
 
 ; export symbols
             XDEF Entry, 
-            XREF __SEG_END_SSTACK, Delay     
+            XREF __SEG_END_SSTACK,      
 
 ; variable/data section
 MY_EXTENDED_RAM:    SECTION
                 ORG   $800
-STEP_SEQ:       dc.b  $0A, $12, $14, $0C
-STEP_Size:      dc.w  $03
 
-; Keyboard Parameters / port_u
+; Stepper Motor Parameters / port_p
+STEP_TAB:       dc.b  $0A, $12, $14, $0C
+Step_Idx:       ds.b  1
+
+;--------Keyboard Parameters / port_u------;
 ; Table with key value
-TAB:            dc.b  $eb, $77, $7b, $7d, $b7, $bb, $bd, $d7, $db, $dd, $e7, $ed, $7e, $be, $de, $ee
+KEY_TAB:        dc.b  $eb, $77, $7b, $7d, $b7, $bb, $bd, $d7, $db, $dd, $e7, $ed, $7e, $be, $de, $ee
 Scan_KeyRow:    dc.b  $70, $B0, $D0, $E0    ; Scan Sequence for Keyboard
-Scan_Count:     ds.w  $01                   ; Index when Scanning Keyboard
+Scan_Count:     ds.w  1                     ; Index when Scanning Keyboard
 key_press:      ds.b  1                     ; Indicator that a key has been recently pressed
 key_val:        ds.b  1                     ; Key Value
 
-MY_CONSTANT_ROM: SECTION
 
-; Port u / Keypad Initialization
+MY_CONSTANT_ROM: SECTION
+;--------Port settings------------;
+; Keyboard Parameters / port_u
 port_u:         equ  $268                  
 ddr_u:          equ  $26A                   
 psr_u:          equ  $26D
 peru_u:         equ  $26C
+;
+; Stepper Motor / port_p
+port_p:     equ  $258
+ddr_p:      equ  $25A
 
 ; Code section
 MyCode:             SECTION
 Entry:
             LDS     #__SEG_END_SSTACK       ; Initialize stack
-            ; Interrupts Setup
-            MOVB    #$80, CRGINT            ; Enable RTI
-            MOVB    #$40, RTICLR            ; Set RTI to 1 ms
-        
-            ; Keyboard Setup
+            
+            ;-----Stepper Moto Setup--------
+            MOVB    #$3E, ddr_p             ; port p / Stepper motor setup
+            MOVB    #$00, Step_Idx          ; Stepper motor Sequence Index
+
+            ;-------Keyboard Setup----------
             BSET    ddr_u, #$F0             ; port u (keyboard) / DDR, set as input
             BSET    peru_u, #$0F            ; port u (keyboard) / PER, Set as pull-up/down
             BSET    psr_u, #$F0             ; port u (keyboard) / PDR, set as pull-up
             MOVW    #$00, Scan_Count        ; initialize the counter
+            
+            ;-------Interrupts Setup--------
+            MOVB    #$80, CRGINT            ; Enable RTI
+            MOVB    #$40, RTICLR            ; Set RTI to 1 ms
             CLI                             ; Initialize Interrupts
+        
 
 ;--------------------------------
 ;           INTERRUPTS
@@ -68,11 +81,13 @@ END_RTI:    BCLR    CRGFLG, #$80        ;
 ;--------------------------------
 ;    KEYBOARD IMPLEMENTATION
 ;--------------------------------
+
+; 1 ms delay for debouncing is needed
 Set_seq:    LDX     Scan_Count          ; Load Scan_Count 
-            LDAA    Scan_KeyRow, X         ; Load keyboard sequence
+            LDAA    Scan_KeyRow, X      ; Load keyboard sequence
             STAA    port_u              ; Write to Keyboard
-            INX                         ;     (Read will happen in 
-            CPX     $04                 ;       next interrupts)
+            INX                         ;   (Read will happen in 
+            CPX     $04                 ;     next interrupts)
             BNE     Exit_set            ; If Scan_Count not maxed, skip
             LDX     #$00                ; If Scan_Count maxed, reset
 Exit_set:   STX     Scan_Count          ; Store Scan_Count
@@ -82,7 +97,7 @@ Scan_seq:   BRCLR   port_u, $0F, Key_Tab; If Key is pressed, go to Key_tab
             RTS
 
 Key_Tab:    MOVB    #$00, key_val       ; Set key_val to 0, & use as index
-Key_Lp:     LDAA    TAB+key_val         ; Load key value and    
+Key_Lp:     LDAA    KEY_TAB+key_val     ; Load key value and    
             CMPA    port_u              ;    compare to port_u 
             BEQ     Exit_key            ; If match move to exit (key_val is set)
             INC     key_val             ; If no match, increase
@@ -90,3 +105,25 @@ Key_Lp:     LDAA    TAB+key_val         ; Load key value and
             MOVB    #$FF, key_val       ; If no match, key_val=$FF
 Exit_key:   RTS
 
+;--------------------------------
+;        STEPPER MOTOR 
+;--------------------------------
+
+; 30 ms Delay between each step
+CW_Step:    LDAB    Step_Idx            ; If Step_Idx >= $04
+            CMPB    $04                 ;    then set to zero
+            BLO     CW_Lp               ; Otherwise, skip
+            MOVB    #$00, Step_Idx      ; 
+CW_Lp:      LDAA    STEP_SEQ+Step_Idx   ; Read Sequence and write 
+            STAA    port_p              ;    it to Stepper Motor
+            INC     Step_Idx            ; Update Index for next time
+            RTS
+
+CCW_Step:   LDAB    Step_Idx            ; If Step_Idx >= $04
+            CMPB    $00                 ;    then set to zero
+            BLO     CW_Lp               ; Otherwise, skip
+            MOVB    #$04, Step_Idx      ; 
+CCW_Lp:     LDAA    STEP_SEQ+Step_Idx   ; Read Sequence and write 
+            STAA    port_p              ;    it to Stepper Motor
+            DEC     Step_Idx            ; Update Index for next time
+            RTS
