32a2cf857a5f047950dd34cf46845d4a40cabd82
fede3alvarez <fede3alvarez@gmail.com>
2020-04-25 15:19:09 -0400

    Separe main into main & interrupts

---
+++ ECE362/jurassic_code/Sources/interrupt.asm
@@ -0,0 +1,99 @@
+; Include derivative-specific definitions
+            INCLUDE 'derivative.inc'
+
+; export symbols
+            XDEF RTI_ISR, IRQ_ISR
+
+            XREF __SEG_END_SSTACK,
+            ;
+            ;----Interrupt--------------
+            RTI_Cnter,
+            ;
+            ;-----Keyboard ------------- 
+            port_u,                     
+            Scan_Count, Scan_KeyRow, 
+            key_val, KEY_TAB,
+            ;
+            ;-----STEPPER MOTOR---------
+            port_p,
+            Step_Idx, STEP_TAB
+
+
+
+;--------------------------------
+;          SUB-ROUTINES
+;--------------------------------
+
+;--------------------------------
+;           INTERRUPTS
+;--------------------------------
+
+RTI_ISR:    LDX     RTI_Cnter           ; Load counter to Idx X
+            INX                         ; Increment Counter
+            STX     RTI_Cnter           ; Store Counter
+            CPX     #1000               ; Has the counter reached value?
+            BNE     END_RTI             ; If not, exit RTI
+            MOVW    #0, RTI_Cnter       ; Else, Initialize counter to 0
+            
+END_RTI:    BCLR    CRGFLG, #$80        ;
+            RTI
+            
+IRQ_ISR:    LDX     RTI_Cnter           ; Load counter to Idx X
+            INX                         ; Increment Counter
+            STX     RTI_Cnter           ; Store Counter
+            CPX     #1000               ; Has the counter reached value?
+            BNE     END_RTI             ; If not, exit RTI
+            MOVW    #0, RTI_Cnter       ; Else, Initialize counter to 0
+            
+END_IRQ:    BCLR    CRGFLG, #$80        ;
+            RTI
+
+;--------------------------------
+;    KEYBOARD IMPLEMENTATION
+;--------------------------------
+;
+; 1 ms delay for debouncing is needed
+Set_seq:    LDX     Scan_Count          ; Load Scan_Count 
+            LDAA    Scan_KeyRow, X      ; Load keyboard sequence
+            STAA    port_u              ; Write to Keyboard
+            INX                         ;   (Read will happen in 
+            CPX     $04                 ;     next interrupts)
+            BNE     Exit_set            ; If Scan_Count not maxed, skip
+            LDX     #$00                ; If Scan_Count maxed, reset
+Exit_set:   STX     Scan_Count          ; Store Scan_Count
+            RTS
+
+Scan_seq:   BRCLR   port_u, $0F,Key_Read; If Key is pressed, go to Key_Read
+            RTS
+
+Key_Read:   MOVB    #$00, key_val       ; Set key_val to 0, & use as index
+Key_Lp:     LDAA    KEY_TAB+key_val     ; Load key value and    
+            CMPA    port_u              ;    compare to port_u 
+            BEQ     Exit_key            ; If match move to exit (key_val is set)
+            INC     key_val             ; If no match, increase
+            BRCLR   key_val,#$10,Key_Lp ;    key_val and try again
+            MOVB    #$FF, key_val       ; If no match, key_val=$FF
+Exit_key:   RTS
+
+;--------------------------------
+;        STEPPER MOTOR 
+;--------------------------------
+
+; 30 ms Delay between each step
+CW_Step:    LDAB    Step_Idx            ; If Step_Idx >= $04
+            CMPB    $04                 ;    then set to zero
+            BLO     CW_Lp               ; Otherwise, skip
+            MOVB    #$00, Step_Idx      ; 
+CW_Lp:      LDAA    STEP_TAB+Step_Idx   ; Read Sequence and write 
+            STAA    port_p              ;    it to Stepper Motor
+            INC     Step_Idx            ; Update Index for next time
+            RTS
+
+CCW_Step:   LDAB    Step_Idx            ; If Step_Idx >= $04
+            CMPB    $00                 ;    then set to zero
+            BLO     CW_Lp               ; Otherwise, skip
+            MOVB    #$04, Step_Idx      ; 
+CCW_Lp:     LDAA    STEP_TAB+Step_Idx   ; Read Sequence and write 
+            STAA    port_p              ;    it to Stepper Motor
+            DEC     Step_Idx            ; Update Index for next time
+            RTS
--- ECE362/jurassic_code/Sources/main.asm
+++ ECE362/jurassic_code/Sources/main.asm
@@ -2,14 +2,27 @@
             INCLUDE 'derivative.inc'
 
 ; export symbols
-            XDEF Entry, RTI_ISR, IRQ_ISR 
+            XDEF Entry,
+            ;
+            ;----Interrupt--------------
+            RTI_Cnter,
+            ;
+            ;-----Keyboard ------------- 
+            port_u,                     
+            Scan_Count, Scan_KeyRow, 
+            key_val, KEY_TAB,
+            ;
+            ;-----STEPPER MOTOR---------
+            port_p,
+            Step_Idx, STEP_TAB
+
             XREF __SEG_END_SSTACK,      
 
 ; variable/data section
 MY_EXTENDED_RAM:    SECTION
                 ORG   $800
 ;-------------ISR Parameters---------------;
-Counter:        ds.w    1
+RTI_Cnter:        ds.w    1
 
 ;---Stepper Motor Parameters / port_p------;
 STEP_TAB:       dc.b    $0A, $12, $14, $0C
@@ -41,7 +54,7 @@ MyCode:             SECTION
 Entry:
             LDS     #__SEG_END_SSTACK       ; Initialize stack
             
-            ;-----Stepper Moto Setup--------;
+            ;-----Stepper Motor; Setup--------;
             MOVB    #$3E, ddr_p             ; port p / Stepper motor setup
             MOVB    #$00, Step_Idx          ; Stepper motor Sequence Index
 
@@ -56,77 +69,7 @@ Entry:
             MOVB    #$40, RTICTL            ; Set RTI to 1 ms
             CLI                             ; Initialize Interrupts
         
+Main_Loop:  NOP                             
+            BRA     Main_Loop
 
-;--------------------------------
-;           INTERRUPTS
-;--------------------------------
-
-RTI_ISR:    LDX     Counter             ; Load counter to Idx X
-            INX                         ; Increment Counter
-            STX     Counter             ; Store Counter
-            CPX     #1000               ; Has the counter reached value?
-            BNE     END_RTI             ; If not, exit RTI
-            MOVW    #0, Counter         ; Else, Initialize counter to 0
-            
-END_RTI:    BCLR    CRGFLG, #$80        ;
-            RTI
-            
-IRQ_ISR:    LDX     Counter             ; Load counter to Idx X
-            INX                         ; Increment Counter
-            STX     Counter             ; Store Counter
-            CPX     #1000               ; Has the counter reached value?
-            BNE     END_RTI             ; If not, exit RTI
-            MOVW    #0, Counter         ; Else, Initialize counter to 0
-            
-END_IRQ:    BCLR    CRGFLG, #$80        ;
-            RTI
-
-;--------------------------------
-;    KEYBOARD IMPLEMENTATION
-;--------------------------------
-;
-; 1 ms delay for debouncing is needed
-Set_seq:    LDX     Scan_Count          ; Load Scan_Count 
-            LDAA    Scan_KeyRow, X      ; Load keyboard sequence
-            STAA    port_u              ; Write to Keyboard
-            INX                         ;   (Read will happen in 
-            CPX     $04                 ;     next interrupts)
-            BNE     Exit_set            ; If Scan_Count not maxed, skip
-            LDX     #$00                ; If Scan_Count maxed, reset
-Exit_set:   STX     Scan_Count          ; Store Scan_Count
-            RTS
-
-Scan_seq:   BRCLR   port_u, $0F, Key_Tab; If Key is pressed, go to Key_tab
-            RTS
-
-Key_Tab:    MOVB    #$00, key_val       ; Set key_val to 0, & use as index
-Key_Lp:     LDAA    KEY_TAB+key_val     ; Load key value and    
-            CMPA    port_u              ;    compare to port_u 
-            BEQ     Exit_key            ; If match move to exit (key_val is set)
-            INC     key_val             ; If no match, increase
-            BRCLR   key_val,#$10,Key_Lp ;    key_val and try again
-            MOVB    #$FF, key_val       ; If no match, key_val=$FF
-Exit_key:   RTS
-
-;--------------------------------
-;        STEPPER MOTOR 
-;--------------------------------
-
-; 30 ms Delay between each step
-CW_Step:    LDAB    Step_Idx            ; If Step_Idx >= $04
-            CMPB    $04                 ;    then set to zero
-            BLO     CW_Lp               ; Otherwise, skip
-            MOVB    #$00, Step_Idx      ; 
-CW_Lp:      LDAA    STEP_TAB+Step_Idx   ; Read Sequence and write 
-            STAA    port_p              ;    it to Stepper Motor
-            INC     Step_Idx            ; Update Index for next time
-            RTS
-
-CCW_Step:   LDAB    Step_Idx            ; If Step_Idx >= $04
-            CMPB    $00                 ;    then set to zero
-            BLO     CW_Lp               ; Otherwise, skip
-            MOVB    #$04, Step_Idx      ; 
-CCW_Lp:     LDAA    STEP_TAB+Step_Idx   ; Read Sequence and write 
-            STAA    port_p              ;    it to Stepper Motor
-            DEC     Step_Idx            ; Update Index for next time
-            RTS
+            END
